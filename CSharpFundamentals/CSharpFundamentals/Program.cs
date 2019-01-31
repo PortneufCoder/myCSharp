dbf5b478a207e1a1586495892d1349756296b328
Victor Ajani <portneufcoder@gmail.com>
2019-01-30 20:21:18 -0500

    Some more C# practice

---
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CSharpFundamentals;
+using CSharpFundamentals.Math;
+
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var victor = new Person();
+            victor.FirstName = "Albert";
+            victor.LastName = "Einstein";
+            victor.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(3, 4);
+            Console.WriteLine(result);
+
+            int[] myArray = new int[4] { 5, 7, 9, 12 };
+            Console.WriteLine(myArray[1]);
+        }
+    }
+}
