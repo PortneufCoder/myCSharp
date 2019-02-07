7c76731009a719a0aa08192805c0385e2392beef
Victor Ajani <portneufcoder@gmail.com>
2019-02-06 21:32:11 -0500

    tyoe-safe

--- CSharpFundamentals/CSharpFundamentals/SpeedCamera.cs
+++ CSharpFundamentals/CSharpFundamentals/SpeedCamera.cs
@@ -13,7 +13,7 @@ namespace CSharpFundamentals
             Console.WriteLine("Now enter the speed of your car: ");
             var carSpeed = Convert.ToInt32(Console.ReadLine() + "km");
 
-            var demeritPoints = 0;
+            int demeritPoints = 0;
 
             if (carSpeed < speedLimit)
             {
