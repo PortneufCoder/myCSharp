95b8d103aee66074038585283fe027bb3e636a9f
Victor Ajani <portneufcoder@gmail.com>
2019-02-08 20:19:05 -0500

    Factorial test

---
+++ CSharpFundamentals/CSharpFundamentals/Games/GetFactorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace CSharpFundamentals.Games
+{
+    public class GetFactorial
+    {
+        public void CalculateFactorial()
+        {
+            Console.WriteLine("Please enter a number: ");
+            var highestFactorial = Convert.ToInt32(Console.ReadLine());
+
+            var lowestFactorial = 1;
+
+            for (var i = 1; i <= highestFactorial; i++)
+            {
+                lowestFactorial *= i;
+
+                Console.WriteLine("{0}! = {1}", highestFactorial, lowestFactorial);
+            }
+        }
+    }
+}
