dfb779e3c9814ba816690b64d17d54432a19cf34
Victor Ajani <portneufcoder@gmail.com>
2019-03-02 19:31:27 -0500

    Some more exercises

---
+++ CSharpFundamentals/TextExercises/ConsecutiveNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace TextExercises
+{
+public class ConsecutiveNumbers
+{
+
+public void NumberSolution()
+{
+    Console.WriteLine("Please enter some numbers:");
+    var input = Console.ReadLine();
+    var listOfNumbers = new List<int>();
+
+    foreach (var number in input.Split('-'))
+    {
+        listOfNumbers.Add(Convert.ToInt32(number));
+
+       listOfNumbers.Sort();
+
+       var isConsecutive = true;
+
+       for (var i = 1; i < listOfNumbers.Count; i++)
+       {
+           if (listOfNumbers[i] != listOfNumbers[i - 1] + 1)
+           {
+               isConsecutive = false;
+               break;
+           }
+       }
+
+       var output = isConsecutive ? "Consecutive" : "Not Consecutive";
+       Console.WriteLine(output);
+    }
+}
+}
+}
